a14ee4c139c0cd185e51895547b3a20710b75c03
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-08-06 11:16:26 +0600

    Building tthe backend section done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import "./App.css";
 import axios, { AxiosError } from 'axios';
+import { CanceledError } from "./services/api-client";
 //import Alert from "./components/Alert";
 //import Exercise1 from "./components/Exercise1";
 //import Button from "./components/Exercise1";
@@ -20,6 +21,9 @@ import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import categories from "./expense-tracker/categories";
 import ProductList from "./components/ProductList";
+import { original } from "immer";
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 /*
   {
     alertVisible && (
@@ -53,21 +57,11 @@ import ProductList from "./components/ProductList";
       <ButtonEx onClick={() => {}}>My Button</ButtonEx>
       <Icon onClick={()=>console.log("Clicked")}/>
  */
-interface User { 
-  id: number;
-  name: string;
-}
+
 
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  useEffect(() => { 
-    axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then((response) => setUsers(response.data))
-      .catch((error) => setError(error.message))
-  },[error])
+  const { users, error, isLoading,setUsers,setError } = useUsers();
   /*const ref = useRef<HTMLInputElement>(null);*/
  /* const [category, setCategory] = useState('');*/
   /*let items = ["New York", "San Francisco", "Tokyo", "Paris"];
@@ -84,11 +78,53 @@ function App() {
  ]);
   //const [cartItems,setCartItems] = useState(['Product 1','Product 2']);
   const visibleExpenses = selectedCategory ? expenses.filter((e) => e.category === selectedCategory) : expenses;
+  const deleteUser = (user: User) => { 
+    const originalUsers = [...users];
+    setUsers(users.filter(u => u.id !== user.id));
+    
+      userService.delete(user.id).catch(err => { 
+        setError(err.message);
+        setUsers(originalUsers);
+      })
+  }
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: 'Mosh' };
+    setUsers([...users, newUser]);
+  
+    userService.create(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch (err => { 
+    setError(err.message);
+    setUsers(originalUsers);
+  });
+  }
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + '!' };
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+
+   
+      userService.update(updatedUser).catch(err => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  }
   return (
     <>
-      { error && <p className="text-danger">{ error}</p>}
-      <ul>
-      {users.map((user) => <li key={user.id}>{ user.name}</li>)}
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+      <ul className="list-group">
+        {users.map((user) => (<li key={user.id} className="list-group-item d-flex justify-content-between">
+          {user.name}
+          <div>
+            <button className="btn btn-outline-secondary mx-1" onClick={()=>updateUser(user)}>Update</button>
+          <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+          </div>
+          
+        </li>
+        ))}
       </ul> 
     </>
  
